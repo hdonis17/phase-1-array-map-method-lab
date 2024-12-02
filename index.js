764ad4414886cdde92409bef842a4a693e7249e4
Harrison Donis <hdonis17@gmail.com>
2024-12-02 13:03:56 -0500

    completes lab

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+function titleCased(){
+  return tutorials.map(tutorial => {
+    const tutorialsSplit = tutorial.split(" ");
+    const titleCasedTutorials = tutorialsSplit.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return titleCasedTutorials.join(" ");
+  });
+
+};
+
+console.log(titleCased());
